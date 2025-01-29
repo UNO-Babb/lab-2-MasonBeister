872a82b5f7b2f06dbca3f1f8243f913b5500aaf5
MasonBeister <mbeister@unomaha.edu>
2025-01-29 15:30:14 +0000

    8 Ball

--- Magic8Ball.py
+++ Magic8Ball.py
@@ -10,9 +10,32 @@ def main():
   #Create a list of your responses.
   print("Magic 8 Ball")
   #Prompt the user for their question.
-
+  answers =["As I see it, yes.", "Ask again later.", 
+  "Better not tell you now.", 
+  "Cannot predict now.", 
+  "Concentrate and ask again.", 
+  "Don’t count on it.", 
+  "It is certain.", 
+  "It is decidedly so.", 
+  "Most likely.", 
+  "My reply is no.", 
+  "My sources say no.", 
+  "Outlook not so good.", 
+  "Outlook good.", 
+  "Reply hazy, try again.", 
+  "Signs point to yes.", 
+  "Very doubtful.", 
+  "Without a doubt.", 
+  "Yes.",
+  "Yes – definitely.",
+  "You may rely on it."]
   #Answer question randomly with one of the options from your earlier list.
+  num = random.random()
+  num = num * 100
+  num = int(num)
+  num = num % 20
 
-
+  question = input("Ask me a yes or no question: ")
+  print(answers[num])
 if __name__ == '__main__':
   main()
